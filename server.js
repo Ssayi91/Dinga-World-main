50ff955122481756cc8c8a65ae726d0ec38aefdc
Sayi <sonny.sayi@gmail.com>
2024-08-17 14:02:44 +0300

    atlas and mongodb settings

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('Connected to MongoDB'))
+      .catch(err => console.error('Could not connect to MongoDB', err));
 
 const app = express();
 const port = 3000;
